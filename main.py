8d2904d6085c373d7eb13a53ea9b49b34f9d2846
BrooksConway <brooksconway@unomaha.edu>
2025-04-18 04:14:49 +0000

    Done

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-#MapPlot.py
-#Name:
-#Date:
-#Assignment:
+# Name:
+# Date:
+# Assignment:
+
+import labor
+import pandas as pd
+import matplotlib.pyplot as plt
+
+result = labor.get_result()
+
+years = []
+emplys = []
+
+for Lab in result:
+    year = Lab["Time"]["Year"]
+    emply = Lab["Data"]["Employed"]["White"]["Counts"]["All"]
+    years.append(year)
+    emplys.append(emply)
+
+# Create a DataFrame using a dictionary of lists
+df = pd.DataFrame({
+    "Year": years,
+    "emply": emplys
+})
+
+print(df)
+
+# Create a scatter plot
+df.plot(kind="scatter", x="Year", y="emply")
+
+# Save the plot to a file
+plt.savefig("output.png")
